d91fc34de4786f226e91d6bf2432e3e0d9cd6831
DESKTOP-MLJ6HR1\Daw2 <raredown@gmail.com>
2017-02-23 20:05:36 +0100

    empenzado

---
+++ CrudHibernateJsfAjax/src/main/java/es/albarregas/modelo/Persona.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Daw2
+ */
+@Entity
+@Table(name = "personas")
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int idPerso;
+    private String nombre;
+    private String apellidos;
+    private String email;
+
+    public int getIdPerso() {
+        return idPerso;
+    }
+
+    public void setIdPerso(int idPerso) {
+        this.idPerso = idPerso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+
+}
